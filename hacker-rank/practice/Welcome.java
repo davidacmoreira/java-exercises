99fc60513f656467863e218012a71a19d6dc29c7
David Moreira <davidmoreira37@gmail.com>
2020-11-20 21:18:52 +0000

    Add HackerRank exercise solution

--- hacker-rank/practice/Welcome.java
+++ hacker-rank/practice/Welcome.java
@@ -1,3 +1,17 @@
+/**
+ * Input Format
+ *   There is no input for this challenge.
+ * 
+ * Output Format
+ *  You must print two lines of output:
+ *     Print Hello, World. on the first line.
+ *     Print Hello, Java. on the second line.
+ * 
+ * Sample Output
+ *   Hello, World.
+ *   Hello, Java.
+ */
+
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
