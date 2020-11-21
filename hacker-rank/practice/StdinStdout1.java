4311222825f2dcef7dcf763fbb0f0bdf5cb9b233
David Moreira <davidmoreira37@gmail.com>
2020-11-21 22:35:24 +0000

    Add HackerRank exercise solution

--- hacker-rank/practice/StdinStdout1.java
+++ hacker-rank/practice/StdinStdout1.java
@@ -1,28 +1,34 @@
-import java.util.Scanner;
-
 /**
+ * Task
+ *   In this challenge, you must read integers from stdin and then print them to stdout.
+ *   Each integer must be printed on a new line. To make the problem a little easier, 
+ *   a portion of the code is provided for you in the editor below.
+ * 
  * Input Format
- *
- * There are 3 lines of input, and each line contains a single integer.
+ *   There are 3 lines of input, and each line contains a single integer.
  *
  * Sample Input
- * 42
- * 100
- * 125
+ *   42
+ *   100
+ *   125
  *
  * Sample Output
- * 42
- * 100
- * 125
+ *   42
+ *   100
+ *   125
  */
+
+import java.util.Scanner;
 
 public class StdinStdout1 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        scan.close();
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        
+        scanner.close();
 
         System.out.println(a);
         System.out.println(b);
